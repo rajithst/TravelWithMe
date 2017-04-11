8f86049cf658233aa1db8359d41a0619aa973f2b
rajithst <rajiththennakoonshady@gmail.com>
2017-04-11 19:38:45 +0530

    viewEncapsulation done

--- AngularFront/src/app/components/home/home.component.ts
+++ AngularFront/src/app/components/home/home.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , ViewEncapsulation } from '@angular/core';
 import {AuthService} from '../../services/auth.service';
 import {Auth0Service} from '../../services/auth0.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class HomeComponent implements OnInit {
 
