babdc8a3cd70ccd9fcfa6432bef59274e93cf103
rajithst <rajiththennakoonshady@gmail.com>
2017-04-26 12:26:16 +0530

    google maps rest api added,places api output results to newsfeed

--- AngularFront/src/app/components/newsfeed/newsfeed.component.ts
+++ AngularFront/src/app/components/newsfeed/newsfeed.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-newsfeed',
@@ -7,9 +8,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewsfeedComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService,) { }
+
+  user: any;
+  profile: any;
+  userid:any;
+  bodyClasses:string = "fixed-sn blue-skin";
+  friends:any;
+  nearby:any;
+  results:any;
+  followers:any;
+
 
   ngOnInit() {
+
+    this.profile = JSON.parse(localStorage.getItem('profile'));
+    const data = {
+      id: this.profile.identities[0].user_id,
+      provider: this.profile.identities[0].provider,
+      userid: this.profile.user_id
+    };
+
+    this.authService.checkId(data).subscribe(res => {
+      this.user = res.data;
+      this.user.me = this.user.identities[0].user_id;
+      console.log(this.user);
+
+
+      /*friends object*/
+      this.friends = this.user.context.mutual_friends.data;
+      this.friends.followers = this.user.personal.followeusers;
+
+      /*places*/
+
+      this.nearby = this.user.nearby.results;
+      console.log(this.nearby)
+
+
+    });
+
+
   }
 
 }
