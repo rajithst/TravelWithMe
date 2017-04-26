d8fdafdda62ebdf58f5286670cf61fa27c297ba4
Madura Herath <maduraherath8@gmail.com>
2017-04-26 11:53:30 +0530

    adding newsfeed suggestarea

---
+++ AngularFront/src/app/components/suggestionarea/suggestionarea.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-suggestionarea',
+  templateUrl: './suggestionarea.component.html',
+  styleUrls: ['./suggestionarea.component.css']
+})
+export class SuggestionareaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ AngularFront/src/app/components/suggestionarea/suggestplaces/suggestplaces.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-suggestplaces',
+  templateUrl: './suggestplaces.component.html',
+  styleUrls: ['./suggestplaces.component.css']
+})
+export class SuggestplacesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
