3a72ed28a86073014571ac11fc05b8b781085a7c
rajithst <rajiththennakoonshady@gmail.com>
2017-05-12 17:02:07 +0530

    features

--- models/blog.js
+++ models/blog.js
@@ -21,24 +21,19 @@ const Blogtab =  module.exports = mongoose.model('Blogtab',blogSchema);
 
 module.exports.addBlogpost = function (Stu,callback) {
 
-    var dt=new Date();//current date and time of server
-    var text = "";//random text
-    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    for( var i=0; i < 5; i++ )
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    var dt=new Date();
+    var text = "featured_image_";
     var pos=Stu.student_img.indexOf(",");
+
     var base64d=Stu.student_img.substring(pos+1);
-    var path="./public/images/"+text+dt.getDate()+dt.getMonth()+dt.getMilliseconds()+".png";
+    var path="./uploads/images/blog/"+text+dt.getDate()+dt.getMonth()+dt.getMilliseconds()+".png";
     var path1="/images/"+text+dt.getDate()+dt.getMonth()+dt.getMilliseconds()+".png";
     fs.writeFile(path,base64d,'base64',function(err){
         if(err) {
             return console.log(err);
         }
-        console.log("The file was saved!");
+
     });
-
-
-
 
     //blogdata.save(callback);
 };
