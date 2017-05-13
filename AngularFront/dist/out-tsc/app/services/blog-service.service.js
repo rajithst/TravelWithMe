1ed003215c33a8635641b6a4febae198b2cb0965
Madura Herath <maduraherath8@gmail.com>
2017-05-13 15:33:48 +0530

    add some changes

--- AngularFront/dist/out-tsc/app/services/blog-service.service.js
+++ AngularFront/dist/out-tsc/app/services/blog-service.service.js
@@ -24,7 +24,7 @@ export let BlogServiceService = class BlogServiceService {
     getPosts(id) {
         let headers = new Headers();
         headers.append('Accept', 'application/json');
-        return this.http.get('http://localhost:3000/blog/getPosts/:' + id).map(res => res.json());
+        return this.http.get('http://localhost:3000/blog/getPosts/' + id).map(res => res.json());
     }
 };
 BlogServiceService = __decorate([
--- AngularFront/src/app/components/blog/blog.component.ts
+++ AngularFront/src/app/components/blog/blog.component.ts
@@ -40,7 +40,6 @@ export class BlogComponent implements OnInit {
     // private http:Http,
 
 
-
   ) { }
 
   ngOnInit() {}
