fda01f829452f3689cc5904ca6c883952f4fdd12
rajithst <rajiththennakoonshady@gmail.com>
2017-05-14 20:14:43 +0530

    TAGS ADD DONE

--- AngularFront/src/app/components/navbar/navbar.component.ts
+++ AngularFront/src/app/components/navbar/navbar.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { GoogleAPIService } from '../../services/google-api.service';
 import { Router } from '@angular/router';
 import {Auth0Service} from '../../services/auth0.service'
-
+import { CompleterService, CompleterData } from 'ng2-completer';
 
 @Component({
   selector: 'app-navbar',
@@ -12,6 +12,8 @@ import {Auth0Service} from '../../services/auth0.service'
 export class NavbarComponent implements OnInit {
 
   flocation:any;
+  fautoplace:any;
+  tautoplace:any;
   tlocation:any;
   profile: any;
   actoken: any;
@@ -22,10 +24,15 @@ export class NavbarComponent implements OnInit {
     /*private flashMessage:FlashMessagesService,*/
     private route:Router,
     private auth: Auth0Service,
-    private googleApi:GoogleAPIService
+    private googleApi:GoogleAPIService,
+    private completerService: CompleterService
   ) { }
 
   ngOnInit() {
+    this.fautoplace='';
+    this.tautoplace='';
+
+
     this.profile = JSON.parse(localStorage.getItem('profile'));
     const data = {
       id: this.profile.identities[0].user_id,
@@ -59,6 +66,25 @@ export class NavbarComponent implements OnInit {
     })
 
   }
+
+  addToTagfrom(e){
+    this.fautoplace = e;
+    this.Fresult = '';
+    this.flocation = '';
+    return false;
+  }
+
+
+  addToTagto(e){
+    this.tautoplace = e;
+    this.Tresult = '';
+    this.tlocation = '';
+    return false;
+  }
+
+
+
+
 
 
 
