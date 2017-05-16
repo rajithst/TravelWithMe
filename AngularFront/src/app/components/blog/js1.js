1a62f41ac942bbe30e7ccc887a3ddf1b397cfeef
KavinduChamara <kavindu.ce@gmail.com>
2017-05-16 22:22:20 +0530

    bsns

--- AngularFront/src/app/components/blog/js1.js
+++ AngularFront/src/app/components/blog/js1.js
@@ -1,12 +1,12 @@
 console.log('ssss');
 
 
-$('.chips-initial').material_chip({
-  data: [{
-    tag: 'Tag 1',
-  }, {
-    tag: 'Tag 2',
-  }, {
-    tag: 'Tag 3',
-  }],
-});
+// $('.chips-initial').material_chip({
+//   data: [{
+//     tag: 'Tag 1',
+//   }, {
+//     tag: 'Tag 2',
+//   }, {
+//     tag: 'Tag 3',
+//   }],
+// });
