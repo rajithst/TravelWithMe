b517d94932d1660c39d6c4541ee6319e870987b2
KavinduChamara <kavindu.ce@gmail.com>
2017-05-21 21:52:27 +0530

    bsns

---
+++ AngularFront/src/app/components/business-page-hotels/business-page-hotels.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-business-page-hotels',
+  templateUrl: './business-page-hotels.component.html',
+  styleUrls: ['./business-page-hotels.component.css']
+})
+export class BusinessPageHotelsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
