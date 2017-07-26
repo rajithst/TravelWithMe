b20ff79f4896ab2cd3dcbf0b3defce01f1cfb1ac
Madura Herath <maduraherath8@gmail.com>
2017-07-26 14:38:07 +0530

    add new postview

---
+++ AngularFront/dist/out-tsc/app/components/viewpost/viewpost.component.js
@@ -0,0 +1,30 @@
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+import { Component, ViewEncapsulation } from '@angular/core';
+let ViewpostComponent = class ViewpostComponent {
+    constructor() {
+        this.bodyClasses = "fixed-sn blue-skin";
+    }
+    ngOnInit() {
+        $('body').addClass(this.bodyClasses);
+        $("body").css('background-color', '#ecf0f1');
+    }
+};
+ViewpostComponent = __decorate([
+    Component({
+        selector: 'app-viewpost',
+        styleUrls: ['viewpost.component.css'],
+        templateUrl: './viewpost.component.html',
+        encapsulation: ViewEncapsulation.None
+    }),
+    __metadata("design:paramtypes", [])
+], ViewpostComponent);
+export { ViewpostComponent };
+//# sourceMappingURL=E:/3rd Year Project/webapp/TravelwithMe/AngularFront/src/app/components/viewpost/viewpost.component.js.map
